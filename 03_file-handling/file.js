56d934b9d798640d27cb31b172079384ea06793a
nihal <nihalsheikh585@gmail.com>
2025-12-23 22:33:46 +0530

    file handling: sync -> writeFileSync(), async -> writeFile()

--- 03_file-handling/file.js
+++ 03_file-handling/file.js
@@ -1,8 +1,13 @@
 const fs = require("fs");
 
-/*
+/* Synchronous
 1. fs -> file system
 2. writeFileSync -> method in fs to write on a file,
 3. ("location path & <fileName>", "Content for the file")
 */
 fs.writeFileSync("./test.txt", "Hello There, this is File System");
+
+
+/* Asynchronous
+*/
+fs.writeFile()
